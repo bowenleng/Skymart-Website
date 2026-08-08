fefbee5b0d7d6c25b9a95b6287d6d7e5f92c7b2c
Bowen <lengbowen4@gmail.com>
2026-08-08 18:43:58 -0500

    Initial commit

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function Home() {
+    const [heading, setHeading] = useState('');
+    const [introduction, setIntroduction] = useState('');
+    const [description, setDescription] = useState('');
+
+    return (
+        <div>
+        </div>
+        <div>
+            {}
+        </div>
+        <div>
+            <button id="changeLanguage">中文</button>
+        </div>
+    );
+}
